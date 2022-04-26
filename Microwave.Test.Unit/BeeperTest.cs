d235cff57c0fc59bb4d43ad447ef37e87ee0f18a
LukasM <au617683@post.au.dk>
2022-04-26 12:56:45 +0200

    Add beeper tests

---
+++ Microwave.Test.Unit/BeeperTest.cs
@@ -0,0 +1,27 @@
+﻿using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit;
+
+[TestFixture]
+public class BeeperTest
+{
+	private IOutput output;
+	private Beeper uut;
+
+	[SetUp]
+	public void Setup()
+	{
+		output = Substitute.For<IOutput>();
+		uut = new Beeper(output);
+	}
+
+	[Test]
+	public void Start_BeepOutputThreeTimes()
+	{
+		uut.Start();
+		output.Received(3).OutputLine("* B E E P *");
+	}
+}
